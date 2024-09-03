e98cbe54d33da5a05a2a52868689ad3735ee9fc1
Seeeop2 <seeeop2@gmail.com>
2024-09-03 22:57:00 +0900

    FEAT: MyFilter1, MyFilter2, MyFilter3 클래스 추가 및 필터링 로직 구현
    - MyFilter1, MyFilter2, MyFilter3 클래스 추가함
    - doFilter 메서드에서 필터 호출 로그 출력함
    - FilterChain 클래스의 doFilter 메서드를 이용하여, 다음 필터 또는 최종 자원으로 요청과 응답 전달함

---
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -0,0 +1,18 @@
+package com.cos.jwt.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyFilter1 implements Filter {
+
+    // doFilter 메서드는 요청과 응답을 처리하는 필터링 로직을 정의합니다.
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        // 필터1이 호출되었음을 콘솔에 출력합니다.
+        System.out.println("필터1");
+
+        // 다음 필터 또는 최종 자원으로 요청과 응답을 전달합니다.
+        chain.doFilter(request, response);
+    }
+}
