d448a28cba2fd6eab75b876f780ba015fd23ceff
Seeeop2 <seeeop2@gmail.com>
2024-09-06 00:24:53 +0900

    FEAT: UserDetails 구현을 위한 PrincipalDetails 클래스 추가
    - PrincipalDetails 클래스를 추가하여 UserDetails 인터페이스를 구현함
    - 사용자 권한을 반환하는 getAuthorities() 메서드를 정의함
    - 사용자 비밀번호를 반환하는 getPassword() 메서드를 정의함
    - 사용자 이름을 반환하는 getUsername() 메서드를 정의함
    - 나머지는 메서드는 반환값을 True로 함.

---
+++ src/main/java/com/cos/jwt/config/auth/PrincipalDetails.java
@@ -0,0 +1,61 @@
+package com.cos.jwt.config.auth;
+
+import com.cos.jwt.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+// UserDetails 인터페이스를 구현하는 PrincipalDetails 클래스
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // 사용자 권한을 반환하는 메서드
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        // 사용자 역할 목록을 순회하며 GrantedAuthority 객체를 생성
+        user.getRolesList().forEach(r->{
+            authorities.add(()->r);
+        });
+        // 권한 목록 반환
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
