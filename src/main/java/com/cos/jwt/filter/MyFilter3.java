46e7cbb5ea748be70778dfea8f54d614d3e5df9d
Seeeop2 <seeeop2@gmail.com>
2024-09-05 22:07:23 +0900

    FEAT: MyFilter3 필터 수정하여 POST 요청 처리 및 HeaderAuth 검증 기능 추가
    - MyFilter3 필터에서 POST 요청만 처리하도록 조건을 추가함.
    - Authorization 헤더에서 토큰 값을 가져와 검증하는 로직을 구현함.
    - 인증 성공 시 다음 필터로 요청을 전달하고, 실패 시 "인증 안됨." 메시지를 클라이언트에 응답함.
    - POST 요청이 들어왔음을 로그에 기록함.
    - chain.doFilter(req, res): 요청과 응답을 다음 필터 또는 최종 자원으로 전달하는 메소드로, 필터 체인에서 다음 작업을 수행하게 함.

--- src/main/java/com/cos/jwt/filter/MyFilter3.java
+++ src/main/java/com/cos/jwt/filter/MyFilter3.java
@@ -1,7 +1,10 @@
 package com.cos.jwt.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyFilter3 implements Filter {
 
@@ -9,10 +12,35 @@ public class MyFilter3 implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        // 필터3이 호출되었음을 콘솔에 출력합니다.
-        System.out.println("필터3");
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
 
-        // 다음 필터 또는 최종 자원으로 요청과 응답을 전달합니다.
-        chain.doFilter(request, response);
+        // 토큰 : cos
+        // ID, PW 정상적으로 들어와서 로그인이 완료되면 토큰을 만들어주고 그걸 응답을 해줌.
+        // 그때 토큰이 넘어오면 이 토큰이 내가 만든 토큰이 맞는지만 검증하면 됨. (RSA, HS256)
+        // POST 요청만 처리하도록 조건 설정
+        if (req.getMethod().equals("POST")) {
+
+            // POST 요청이 들어왔음을 로그에 기록
+            System.out.println("POST 요청됨.");
+            // Authorization 헤더에서 토큰 값을 가져옴
+            String headerAuth = req.getHeader("Authorization");
+            // Authorization 헤더의 값 출력
+            System.out.println(headerAuth);
+            // 필터1이 호출되었음을 콘솔에 출력합니다.
+            System.out.println("필터3");
+
+            // 토큰 값이 "cos"인지 검증
+            if (headerAuth.equals("cos")) {
+                // 인증 성공 시 다음 필터 또는 최종 자원으로 요청과 응답을 전달
+                chain.doFilter(req, res);
+
+            // 인증 실패 시 클라이언트에 응답
+            }else {
+                PrintWriter out = res.getWriter();
+                // 인증 실패 메시지 출력
+                out.println("인증 안됨.");
+            }
+        }
     }
 }
