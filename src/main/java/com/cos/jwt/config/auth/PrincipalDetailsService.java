bd864326d96891bd719262590f115772f8ea78ee
Seeeop2 <seeeop2@gmail.com>
2024-09-08 23:20:11 +0900

    REFACTOR: PrincipalDetailsService 클래스 주석 수정 및 로그 추가
    - PrincipalDetailsService 클래스의 주석을 수정하여 기능 설명을 명확하게 함.
    - 사용자 엔티티 조회 후 로그 출력 추가하여 디버깅 용이성을 향상시킴.
    - formLogin 비활성화 시의 동작을 명확히 설명함.

--- src/main/java/com/cos/jwt/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/cos/jwt/config/auth/PrincipalDetailsService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 // http://localhost:8080/login 요청 시, 동작함.
 // 하지만, formLogin을 Disable 해놨기에 동작을 안함.
+// formLogin을 비활성화하더라도, 다른 방식(예: JWT, Basic Auth 등)으로 인증을 처리할 경우 PrincipalDetailsService가 실행될 수 있음.
 @Service
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
@@ -19,10 +20,12 @@ public class PrincipalDetailsService implements UserDetailsService {
     // 사용자 이름으로 사용자를 로드하는 메서드
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 메서드 호출 로그 출력
         System.out.println("PrincipalDetailsService의 loadUserByUsername()");
         // username으로 사용자 엔티티를 조회
         User userEntity = userRepository.findByUsername(username);
-
+        // 조회된 사용자 엔티티 로그 출력
+        System.out.println("userEntity: " + userEntity);
         // 사용자 엔티티를 PrincipalDetails로 감싸서 반환
         return new PrincipalDetails(userEntity);
     }
